0ede9ebb3286d236d84f19540108024d9cddc2c6
Craig <indigomx9@gmail.com>
2022-08-09 22:20:42 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# RTK Query
+
+Initial setup of the project.  Created with TypeScript, Vite, React, JWT, React Router, and Redux-Toolkit RTK Query.
+
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore expedita eveniet dolores? Rerum distinctio repellat assumenda amet dolorum exercitationem sequi aliquid eius omnis quod, mollitia, doloremque, commodi expedita ea! Exercitationem.</p>
+        </React.Fragment>
+    );
+};
+
+
